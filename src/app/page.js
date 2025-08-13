3daa20c3fe00d71a85ba7e61dd4d959b99536904
Hasibur14 <hasib141415@gmail.com>
2025-08-13 12:44:51 +0600

    [ADD] folder and route setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 
+import Home from "@/components/Home/Home";
 
-export default function Home() {
+const MainLayout = () => {
   return (
-    <div className="text-2xl">
-      <h2>  Kabana is now Live.....................!!</h2>
+    <div>
+      <Home />
     </div>
-  );
+  )
 }
+
+export default MainLayout;
